70c66e6aa5c5643ed16ae86182c600be86914d97
thakuranshu-dev <thakuranshu01485@gmail.com>
2026-02-26 19:58:10 +0530

    Add Order Repository and Product Repository

---
+++ src/main/java/com/springeg/ordermanagement/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.springeg.ordermanagement.repository;
+
+import com.springeg.ordermanagement.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
